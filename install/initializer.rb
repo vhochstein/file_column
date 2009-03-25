ddd4cae314d7132ac2a9d7b15dbbc4e863f88db3
Caio Chassot <dev@caiochassot.com>
2009-03-25 04:31:45 -0300

    Added an installer script that installs an initializer with the commented option to change the store path.

---
+++ install/initializer.rb
@@ -0,0 +1,6 @@
+# Uncomment the following lines to have FileColumn store tree placed under
+# public/files instead of directly under public.
+
+# FileColumn::ClassMethods::DEFAULT_OPTIONS.merge! \
+#   :web_root => "files/",
+#   :root_path => File.join(RAILS_ROOT, "public", "files")
